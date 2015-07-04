e882e2dd7f8e695de31c78b00482cf17a2cdaabc
rasmussn <rasmus@uoregon.edu>
2015-07-04 13:46:35 -0700

    Initial version.

---
+++ src/wmpi-sizes.h
@@ -0,0 +1,9 @@
+!
+! This file should be generated from a C file that calculates
+! all of the sizes of the C handle types for a particular
+! library.
+!
+! WARNING: This file is LIBRARY DEPENDENT
+!
+
+integer, parameter :: LEN_MPI_COMM = 8
