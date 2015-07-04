bc4f365502ac81f59b272de07797eeed3465a015
rasmussn <rasmus@uoregon.edu>
2015-07-04 13:47:19 -0700

    Will move to wmpi-handle-sizes.h

--- src/wmpi-sizes.h
+++
@@ -1,9 +0,0 @@
-!
-! This file should be generated from a C file that calculates
-! all of the sizes of the C handle types for a particular
-! library.
-!
-! WARNING: This file is LIBRARY DEPENDENT
-!
-
-integer, parameter :: LEN_MPI_COMM = 8
