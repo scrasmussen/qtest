a714e744ff87cb94f5258cdc7b64e64cb2de80f7
Craig Rasmussen <rasmus@uoregon.edu>
2015-07-06 10:43:10 -0700

    Initial version.

---
+++ test/test-wmpi.F90
@@ -0,0 +1,17 @@
+program test_wmpi_types
+   use ISO_C_BINDING
+   use wmpi_types
+   use wmpi_f2c_interfaces
+   use mpi_f08
+   implicit none
+
+   integer :: rank, ierror
+
+   call MPI_Init()
+
+   call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierror)
+   print *, "(rank,ierror)==", rank, ierror
+
+   call MPI_Finalize()
+
+end program
