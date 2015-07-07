8d2485f99ceaecc07962f613c056c5f2290fc73b
Craig Rasmussen <rasmus@uoregon.edu>
2015-07-07 09:44:51 -0700

    Finished MPI_Send_f08 implementation.

--- src/mpi-f08-ctools.F90
+++ src/mpi-f08-ctools.F90
@@ -54,7 +54,8 @@ end subroutine MPI_Comm_rank_f08
 subroutine MPI_Send_f08(buf,count,datatype,dest,tag,comm,ierror)
   use, intrinsic :: ISO_C_BINDING, only : C_INT
   use :: mpi_f08, only : MPI_Datatype, MPI_Comm
-  use :: wmpi_f2c_interfaces, only : WMPI_Comm_f2c
+  use :: wmpi_ctool_interfaces, only : WMPI_Send
+  use :: wmpi_f2c_interfaces, only : WMPI_Datatype_f2c, WMPI_Comm_f2c
   use :: wmpi_types, only : WMPI_Datatype, WMPI_Comm
   implicit none
   REAL, DIMENSION(*), INTENT(IN) :: buf
@@ -68,10 +69,13 @@ subroutine MPI_Send_f08(buf,count,datatype,dest,tag,comm,ierror)
 
   print *,'MPI_Comm_rank_f08 wrapper before c calls'
 
+  c_count = count
+  c_datatype = WMPI_Datatype_f2c(datatype%MPI_VAL)
+  c_dest = dest
+  c_tag = tag
   c_comm = WMPI_Comm_f2c(comm%MPI_VAL)
 
-  c_ierror = WMPI_Comm_rank(c_comm, c_rank, 1_C_INT)
-  rank = c_rank
+  c_ierror = WMPI_Send(buf,c_count,c_datatype,c_dest,c_tag,c_comm,1_C_INT)
 
   if (present(ierror)) ierror = c_ierror
 
