671183c1e18a8b5ab8c92ed10a52f12bc8e47e32
Craig Rasmussen <rasmus@uoregon.edu>
2015-07-08 10:51:00 -0700

    Initial version.

---
+++ src/wmpi_f.h
@@ -0,0 +1,21 @@
+/*
+ * This file contains function prototypes for the final Fortran C tool wrapper
+ * functions that call the corresponding PMPI Fortran procedures.
+ */
+
+#ifndef WMPI_F_H
+#define WMPI_F_H
+
+#if defined(c_plusplus) || defined(__cplusplus)
+extern "C" {
+#endif
+
+
+void WMPI_Comm_rank_F(int comm, int * rank, int * ierror);
+
+
+#if defined(c_plusplus) || defined(__cplusplus)
+}
+#endif
+
+#endif /* WMPI_F_H */
