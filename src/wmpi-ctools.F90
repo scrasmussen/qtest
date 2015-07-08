324d74af3ddc731462fed2d5024229dcb716e3df
Craig Rasmussen <rasmus@uoregon.edu>
2015-07-08 10:52:27 -0700

    Initial version.

---
+++ src/wmpi-ctools.F90
@@ -0,0 +1,23 @@
+!===== Fortran WRAPPER 2 =====
+
+subroutine WMPI_Comm_rank_F(comm,rank,ierror) &
+  BIND(C,name="WMPI_Comm_rank_F")
+  use, intrinsic :: ISO_C_BINDING, only : C_INT
+  use :: mpi, only : PMPI_Comm_rank
+  implicit none
+  INTEGER(C_INT), INTENT(IN), VALUE :: comm
+  INTEGER(C_INT), INTENT(OUT) :: rank
+  INTEGER(C_INT), INTENT(OUT) :: ierror
+  integer :: f_ierror
+  integer :: f_rank
+
+  print *,'WMPI_Comm_rank_F wrapper before PMPI call'
+
+  call PMPI_Comm_rank(comm, f_rank, f_ierror)
+
+  rank = f_rank
+  ierror = f_ierror
+
+  print *,'WMPI_Comm_rank_F wrapper after PMPI call'
+
+end subroutine WMPI_Comm_rank_F
