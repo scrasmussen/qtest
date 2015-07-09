4777b0b6622701056f28e0d16d45be28b479f00c
Soren Rasmussen <rasmussen24@llnl.gov>
2015-07-09 15:49:45 -0700

    Added MPI send and receive

--- test/test-wmpi.F90
+++ test/test-wmpi.F90
@@ -5,12 +5,31 @@ program test_wmpi_types
    use mpi_f08
    implicit none
 
-   integer :: rank, ierror
+   integer :: rank, ierror, data
+   type(MPI_STATUS) :: stat
 
    call MPI_Init(ierror)
 
    call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierror)
    print *, "(rank,ierror)==", rank, ierror
+
+
+
+   if (rank .eq. 1) then
+      data = 11
+      call MPI_Send(data,1,MPI_INTEGER,0,7,MPI_COMM_WORLD,ierror)
+   else
+      print *, 'BEFORE MPI_RECV'
+      call MPI_Recv(data,1,MPI_INTEGER,1,7,MPI_COMM_WORLD,MPI_STATUS_IGNORE,&
+           ierror)
+      print *, 'AFTER MPI_RECV'
+   end if
+
+   if (rank .eq. 0) then
+      if (data .ne. 11) then
+         print *, 'ERROR: data .ne. 11, data =', data
+      end if
+   end if
 
    call MPI_Finalize(ierror)
 
