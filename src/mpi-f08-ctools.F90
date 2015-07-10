ada073fd8e9d98cd4ec8c4abd8cc16f957c9f7e4
Soren Rasmussen <rasmussen24@llnl.gov>
2015-07-10 15:26:24 -0700

    Inn the process of adding MPI_Recv

--- src/mpi-f08-ctools.F90
+++ src/mpi-f08-ctools.F90
@@ -108,38 +108,46 @@ subroutine MPI_Send_f08(buf,count,datatype,dest,tag,comm,ierror)
 
 end subroutine MPI_Send_f08
 
-!!$subroutine MPI_Recv_f08(buf,count,datatype,source,tag,comm,status,ierror)
-!!$  use, intrinsic :: ISO_C_BINDING, only : C_INT
-!!$  !use :: wmpi_ctool_interfaces, only : WMPI_Recv
-!!$  use :: mpi_f08, only : MPI_Datatype, MPI_Comm, MPI_Status
-!!$  use :: wmpi_types, only : WMPI_Datatype, WMPI_Comm, WMPI_Status
-!!$  use :: wmpi_f2c_interfaces, only : WMPI_Type_f2c, WMPI_Comm_f2c, &
-!!$       WMPI_Status_f2c
-!!$  implicit none
-!!$  REAL, DIMENSION(*), INTENT(IN) :: buf
-!!$  INTEGER, INTENT(IN) :: count, source, tag
-!!$  TYPE(MPI_Datatype), INTENT(IN) :: datatype
-!!$  TYPE(MPI_Comm), INTENT(IN) :: comm
-!!$  TYPE(MPI_Status) :: status
-!!$  INTEGER, OPTIONAL, INTENT(OUT) :: ierror
-!!$  INTEGER(C_INT) :: C_COUNT, C_SOURCE, c_tag, c_ierror
-!!$  TYPE(WMPI_Datatype) :: c_datatype
-!!$  TYPE(WMPI_Comm) :: c_comm
-!!$  !type(WMPI_Status) :: c_status
-!!$
-!!$  print *,'MPI_Recv_f08 wrapper before c calls'
-!!$
-!!$  c_count = count
-!!$  c_datatype = WMPI_Type_f2c(datatype%MPI_VAL)
-!!$  c_source = source
-!!$  c_tag = tag
-!!$  c_comm = WMPI_Comm_f2c(comm%MPI_VAL)
-!!$  !c_status = WMPI_Status_f2c(status) ! WARNING WARNING WARNING !!!!!!!!! FIX THIS !!!!!!!!
-!!$  
-!!$  call PMPI_Recv(buf,count,datatype,source,tag,comm,status,c_ierror)
-!!$  !c_ierror = WMPI_Recv(buf,count,datatype,source,tag,comm,status,1_C_INT)
-!!$
-!!$  if (present(ierror)) ierror = c_ierror
-!!$
-!!$  print *,'MPI_Recv_f08 wrapper after c calls'
-!!$end subroutine MPI_Recv_f08
+subroutine MPI_Recv_f08(buf,count,datatype,source,tag,comm,status,ierror)
+  use, intrinsic :: ISO_C_BINDING, only : C_INT
+  !use :: wmpi_ctool_interfaces, only : WMPI_Recv
+  use :: mpi_f08, only : MPI_Datatype, MPI_Comm, MPI_Status
+  use :: wmpi_types, only : WMPI_Datatype, WMPI_Comm, WMPI_Status
+  use :: wmpi_f2c_interfaces, only : WMPI_Type_f2c, WMPI_Comm_f2c, &
+       WMPI_Status_f2c, WMPI_Status_c2f
+  implicit none
+  REAL, DIMENSION(*), INTENT(IN) :: buf
+  INTEGER, INTENT(IN) :: count, source, tag
+  TYPE(MPI_Datatype), INTENT(IN) :: datatype
+  TYPE(MPI_Comm), INTENT(IN) :: comm
+  TYPE(MPI_Status) :: status
+  INTEGER, OPTIONAL, INTENT(OUT) :: ierror
+  INTEGER(C_INT) :: C_COUNT, C_SOURCE, c_tag, c_ierror
+  TYPE(WMPI_Datatype) :: c_datatype
+  TYPE(WMPI_Comm) :: c_comm
+  TYPE(WMPI_Status) :: c_status
+
+  TYPE(MPI_Status) :: temp
+
+  print *,'MPI_Recv_f08 wrapper before c calls'
+
+  c_count = count
+  c_datatype = WMPI_Type_f2c(datatype%MPI_VAL)
+  c_source = source
+  c_tag = tag
+  c_comm = WMPI_Comm_f2c(comm%MPI_VAL)
+
+  temp = status
+  !ierror = WMPI_Status_f2c(status, c_status) ! WARNING WARNING WARNING !!!!
+  print *, 'ERROR FOR STATUS IS', ierror
+  !ierror = WMPI_Status_c2f(c_status,status) ! WARNING WARNING WARNING !!!!
+  print *, 'ERROR FOR STATUS II IS', ierror  
+
+  
+  call PMPI_Recv(buf,count,datatype,source,tag,comm,temp,ierror)
+  !c_ierror = WMPI_Recv(buf,count,datatype,source,tag,comm,status,1_C_INT)
+
+  if (present(ierror)) ierror = c_ierror
+
+  print *,'MPI_Recv_f08 wrapper after c calls'
+end subroutine MPI_Recv_f08
--- src/wmpi-ctool-interfaces.F90
+++ src/wmpi-ctool-interfaces.F90
@@ -11,7 +11,8 @@ module wmpi_ctool_interfaces
 interface
 
 ! WMPI_Init
-function WMPI_Init(fflag) result(ierror) BIND(C,name='WMPI_Init')
+function WMPI_Init(fflag) result(ierror) &
+  BIND(C,name='WMPI_Init')
   use, intrinsic :: ISO_C_BINDING, only : C_INT
   implicit none
   INTEGER(C_INT), INTENT(IN), VALUE :: fflag
@@ -19,7 +20,8 @@ function WMPI_Init(fflag) result(ierror) BIND(C,name='WMPI_Init')
 end function WMPI_Init
 
 ! WMPI_Finalize
-function WMPI_Finalize(fflag) result(ierror) BIND(C,name='WMPI_Finalize')
+function WMPI_Finalize(fflag) result(ierror) &
+  BIND(C,name='WMPI_Finalize')
   use, intrinsic :: ISO_C_BINDING, only : C_INT
   implicit none
   INTEGER(C_INT), INTENT(IN), VALUE :: fflag
@@ -27,7 +29,8 @@ function WMPI_Finalize(fflag) result(ierror) BIND(C,name='WMPI_Finalize')
 end function WMPI_Finalize
 
 ! WMPI_Comm_rank
-function WMPI_Comm_rank(comm,rank,fflag) result(ierror) BIND(C,name='WMPI_Comm_rank')
+function WMPI_Comm_rank(comm,rank,fflag) result(ierror) &
+  BIND(C,name='WMPI_Comm_rank')
   use, intrinsic :: ISO_C_BINDING, only : C_INT
   use WMPI_types, only : WMPI_Comm
   implicit none
@@ -40,21 +43,37 @@ end function WMPI_Comm_rank
 ! WMPI_Comm_size
 
 ! WMPI_Send
-function WMPI_Send(buf,count,datatype,dest,tag,comm,fflag) result(ierror) BIND(C,name='WMPI_Send')
-   use, intrinsic :: ISO_C_BINDING, only : C_INT
-   use WMPI_types, only : WMPI_Datatype, WMPI_Comm
-   implicit none
+function WMPI_Send(buf,count,datatype,dest,tag,comm,fflag) result(ierror) &
+  BIND(C,name='WMPI_Send')
+  use, intrinsic :: ISO_C_BINDING, only : C_INT
+  use WMPI_types, only : WMPI_Datatype, WMPI_Comm
+  implicit none
 ! TEMPORARY REPLACEMENT ONLY for compilers other than gfortran
 !  TYPE(*), DIMENSION(..), INTENT(IN) :: buf
-   REAL, DIMENSION(*), INTENT(IN) :: buf
-   INTEGER(C_INT), INTENT(IN), VALUE :: count, dest, tag
-   TYPE(WMPI_Datatype), INTENT(IN), VALUE :: datatype
-   TYPE(WMPI_Comm), INTENT(IN), VALUE :: comm
-   INTEGER(C_INT), INTENT(IN), VALUE :: fflag
-   INTEGER(C_INT) :: ierror
+  REAL, DIMENSION(*), INTENT(IN) :: buf
+  INTEGER(C_INT), INTENT(IN), VALUE :: count, dest, tag
+  TYPE(WMPI_Datatype), INTENT(IN), VALUE :: datatype
+  TYPE(WMPI_Comm), INTENT(IN), VALUE :: comm
+  INTEGER(C_INT), INTENT(IN), VALUE :: fflag
+  INTEGER(C_INT) :: ierror
 end function WMPI_Send
 
 ! WMPI_Recv
+function WMPI_Recv(buf,count,datatype,source,tag,comm,status,fflag) &
+  result(ierror) &
+  BIND(C,name='WMPI_Recv')
+  use, intrinsic :: ISO_C_BINDING, only : C_INT
+  use WMPI_types, only : WMPI_Datatype, WMPI_Comm
+  implicit none
+! TEMPORARY REPLACEMENT ONLY for compilers other than gfortran
+!  TYPE(*), DIMENSION(..), INTENT(IN) :: buf
+  REAL, DIMENSION(*), INTENT(IN) :: buf
+  INTEGER(C_INT), INTENT(IN), VALUE :: count, source, tag
+  TYPE(WMPI_DATATYPE), INTENT(IN), VALUE :: datatype
+  TYPE(WMPI_Comm), INTENT(IN), VALUE :: comm
+  TYPE(WMPI_Status) :: status
+  INTEGER(C_INT) :: ierror
+end function WMPI_Recv
 
 end interface
 
