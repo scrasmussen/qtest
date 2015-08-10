28e605d39c50bc283fbc8d8feeaf598b0e452f5c
Craig Rasmussen <rasmus@uoregon.edu>
2015-08-10 11:16:21 -0700

    Completed get_processor_name.

--- test/get_processor_name.F90
+++ test/get_processor_name.F90
@@ -1,23 +1,23 @@
 #define VERBOSE
 
 program test_get_processor_name
-   use mpi_f08
+   use :: mpi_f08, only : MPI_Get_processor_name, MPI_MAX_PROCESSOR_NAME, MPI_SUCCESS
    implicit none
    integer :: err
    character(len=MPI_MAX_PROCESSOR_NAME) :: name
    integer :: resultlen
 
    call MPI_Init(err)
-   if (err != MPI_SUCCESS) stop "ERROR calling MPI_Init"
+   if (err /= MPI_SUCCESS) ERROR STOP "ERROR calling MPI_Init"
 
    call MPI_Get_processor_name(name, resultlen, err)
-   if (err != MPI_SUCCESS) stop "ERROR calling MPI_Get_processor_name"
+   if (err /= MPI_SUCCESS) ERROR STOP "ERROR calling MPI_Get_processor_name"
 
 #ifdef VERBOSE
-   print *, " len = ", resultlen, "name = ", name(1:resultlen)
+   print *, "name = ", TRIM(name), ", len =", resultlen
 #endif
 
-   call MPI_Finalize()
-   if (err != MPI_SUCCESS) stop "ERROR calling MPI_Finalize"
+   call MPI_Finalize(err)
+   if (err /= MPI_SUCCESS) ERROR STOP "ERROR calling MPI_Finalize"
 
 end program
