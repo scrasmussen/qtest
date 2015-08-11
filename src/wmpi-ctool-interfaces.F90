6a2d9306472262c337e5f7a6cfc0d0f4a83006f9
rasmussn <rasmus@uoregon.edu>
2015-08-10 21:50:00 -0700

    Fix for choice buffers.

--- src/wmpi-ctool-interfaces.F90
+++ src/wmpi-ctool-interfaces.F90
@@ -1,3 +1,5 @@
+#define F_INTEROP_TR
+
 !
 ! This file interfaces for WMPI wrappers to ctools
 !   - this file should be generated
@@ -48,9 +50,11 @@ function WMPI_Send(buf,count,datatype,dest,tag,comm,fflag) result(ierror) &
   use, intrinsic :: ISO_C_BINDING, only : C_INT
   use WMPI_types, only : WMPI_Datatype, WMPI_Comm
   implicit none
-! TEMPORARY REPLACEMENT ONLY for compilers other than gfortran
-!  TYPE(*), DIMENSION(..), INTENT(IN) :: buf
+#ifdef F_INTEROP_TR
+  TYPE(*), DIMENSION(..), INTENT(IN) :: buf
+#else
   REAL, DIMENSION(*), INTENT(IN) :: buf
+#endif
   INTEGER(C_INT), INTENT(IN), VALUE :: count, dest, tag
   TYPE(WMPI_Datatype), INTENT(IN), VALUE :: datatype
   TYPE(WMPI_Comm), INTENT(IN), VALUE :: comm
@@ -65,9 +69,11 @@ function WMPI_Recv(buf,count,datatype,source,tag,comm,status,fflag) &
   use mpi_f08, only : MPI_Status
   use WMPI_types, only : WMPI_Datatype, WMPI_Comm, WMPI_Status
   implicit none
-! TEMPORARY REPLACEMENT ONLY for compilers other than gfortran
-!  TYPE(*), DIMENSION(..), INTENT(IN) :: buf
-  REAL, DIMENSION(*), INTENT(OUT) :: buf
+#ifdef F_INTEROP_TR
+  TYPE(*), DIMENSION(..) :: buf
+#else
+  REAL, DIMENSION(*) :: buf
+#endif
   INTEGER(C_INT), INTENT(IN), VALUE :: count, source, tag
   TYPE(WMPI_Datatype), INTENT(IN), VALUE :: datatype
   TYPE(WMPI_Comm), INTENT(IN), VALUE :: comm
